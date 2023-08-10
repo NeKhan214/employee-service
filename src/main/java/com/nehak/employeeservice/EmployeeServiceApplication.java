69b5485ec54dfdc4dfd7f160a761641efdc82e5f
NehaK214 <er.nehakhan@gmail.com>
2023-08-10 22:14:05 +0530

    Spring cloud zipkin and micrometer distributed tracing enabled.

--- src/main/java/com/nehak/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/nehak/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,8 @@
 package com.nehak.employeeservice;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,7 +22,9 @@ public class EmployeeServiceApplication {
 //    public WebClient webClient() {
 //        return WebClient.builder().build();
 //    }
-
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
